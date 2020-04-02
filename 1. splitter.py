3cde384dedc07682e61d15e75cb478026d474537
MUNEEB AHMED <muneebpandith@gmail.com>
2020-04-02 23:17:51 +0530

    Update 1. splitter.py

--- 1. splitter.py
+++ 1. splitter.py
@@ -5,7 +5,7 @@ import http.client
 
 def complete0():
         conn = http.client.HTTPSConnection("api.msg91.com")
-        payload = "{ \"sender\": \"SOCKET\", \"route\": \"4\", \"country\": \"91\", \"sms\": [ { \"message\": \"Splitting process starting\", \"to\": [ \"9149429559\", \"9797091372\] }] }"
+        payload = "{ \"sender\": \"SOCKET\", \"route\": \"4\", \"country\": \"91\", \"sms\": [ { \"message\": \"Splitting process starting\", \"to\": [ \"9149429559\", \"9797091372\" ] }] }"
         headers = {'authkey': "118364AVIfu09J5e85f50eP1",'content-type': "application/json"}
         conn.request("POST", "/api/v2/sendsms", payload, headers)
         res = conn.getresponse()
