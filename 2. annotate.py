e4446d396e2841523e9ef0d4496b5667e0ebc48f
MUNEEB AHMED <muneebpandith@gmail.com>
2020-04-02 21:00:25 +0530

    Update 2. annotate.py

--- 2. annotate.py
+++ 2. annotate.py
@@ -6,6 +6,25 @@ import pandas as pd
 import numpy as np
 import sys
 
+def startinit():
+        conn = http.client.HTTPSConnection("api.msg91.com")
+        payload = "{ \"sender\": \"SOCKET\", \"route\": \"4\", \"country\": \"91\", \"sms\": [ { \"message\": \"Annotprocess starting\", \"to\": [ \"9149429559\", \"9797091372\$
+        headers = {'authkey': "118364AVIfu09J5e85f50eP1",'content-type': "application/json"}
+        conn.request("POST", "/api/v2/sendsms", payload, headers)
+        res = conn.getresponse()
+        data = res.read()
+        print(data.decode("utf-8"))
+       
+
+def complete():
+        conn = http.client.HTTPSConnection("api.msg91.com")
+        payload = "{ \"sender\": \"SOCKET\", \"route\": \"4\", \"country\": \"91\", \"sms\": [ { \"message\": \"Annot process complete\", \"to\": [ \"9149429559\", \"9797091372\$
+        headers = {'authkey': "118364AVIfu09J5e85f50eP1",'content-type': "application/json"}
+        conn.request("POST", "/api/v2/sendsms", payload, headers)
+        res = conn.getresponse()
+        data = res.read()
+        print(data.decode("utf-8"))
+       
 def getannotation(filename):
 	fileobj = open(filename,"r")
 	filedata = fileobj.read()
@@ -118,8 +137,12 @@ def main():
 
 					fileattrib['annotation']=updateannotation(annotationdata,fileattrib['frameno'])
 					annotateimage(readingfrom, savingto, fileattrib)
-								
+	return 1
 if __name__== "__main__":
-	main()
+        startinit()
+        if main()==1 :
+                complete()
+
+
 
 
