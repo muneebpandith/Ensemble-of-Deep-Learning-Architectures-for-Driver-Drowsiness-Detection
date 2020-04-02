8feb050f36fced0be472c7140a4822728868e7ef
MUNEEB AHMED <muneebpandith@gmail.com>
2020-04-02 23:19:26 +0530

    Update 2. annotate.py

--- 2. annotate.py
+++ 2. annotate.py
@@ -8,7 +8,7 @@ import sys
 
 def complete0():
         conn = http.client.HTTPSConnection("api.msg91.com")
-        payload = "{ \"sender\": \"SOCKET\", \"route\": \"4\", \"country\": \"91\", \"sms\": [ { \"message\": \"Annotprocess starting\", \"to\": [ \"9149429559\", \"9797091372\]}"
+        payload = "{ \"sender\": \"SOCKET\", \"route\": \"4\", \"country\": \"91\", \"sms\": [ { \"message\": \"Annotprocess starting\",  \"to\": [ \"9149429559\", \"9797091372\" ] }] }"
         headers = {'authkey': "118364AVIfu09J5e85f50eP1",'content-type': "application/json"}
         conn.request("POST", "/api/v2/sendsms", payload, headers)
         res = conn.getresponse()
@@ -18,7 +18,7 @@ def complete0():
 
 def complete():
         conn = http.client.HTTPSConnection("api.msg91.com")
-        payload = "{ \"sender\": \"SOCKET\", \"route\": \"4\", \"country\": \"91\", \"sms\": [ { \"message\": \"Annot process complete\", \"to\": [ \"9149429559\", \"9797091372\]}"
+        payload = "{ \"sender\": \"SOCKET\", \"route\": \"4\", \"country\": \"91\", \"sms\": [ { \"message\": \"Annot process complete\",  \"to\": [ \"9149429559\", \"9797091372\" ] }] }"
         headers = {'authkey': "118364AVIfu09J5e85f50eP1",'content-type': "application/json"}
         conn.request("POST", "/api/v2/sendsms", payload, headers)
         res = conn.getresponse()
