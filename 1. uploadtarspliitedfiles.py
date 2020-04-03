f7e148de13cd8c121a5fc75d7f64876cbe617bc4
MUNEEB AHMED <muneebpandith@gmail.com>
2020-04-03 14:06:18 +0530

    Update 1. uploadtarspliitedfiles.py

--- 1. uploadtarspliitedfiles.py
+++ 1. uploadtarspliitedfiles.py
@@ -1,6 +1,6 @@
 import http.client
 import os
-
+import dropbox
 
 def complete0():
         conn = http.client.HTTPSConnection("api.msg91.com")
@@ -20,11 +20,19 @@ def complete():
 	data = res.read()
 	print(data.decode("utf-8"))
 
+def upload_file(file_from, file_to,access_token):
+	dbx = dropbox.Dropbox(access_token)
+	f = open(file_from, 'rb')
+	dbx.files_upload(f.read(), file_to)
 
 def main():
-  #this excludes subject 005
-  os.system("tar -czvf '../../MTECHMINORLINUX/DATA/newsubjectwise.tar.gz' '../../MTECHMINORLINUX/DATA/newsubjectwise' >> tarofsplittedfiles.txt")
-  return 1
+	#this excludes subject 005
+	
+	access_token = 'pf60SIufNzAAAAAAAAAAQzR_9Az9uueqBoo5ueYoErVq-SQ_U1iKYnpHcoBPfTDf'
+	file_from = '../../MTECHMONORLINUX/DATA/newsubjectwise.tar.gz'  
+	file_to = '/Nexus/newsubjectwise.tar.gz' 
+	upload_file(file_from,file_to,access_token)
+	return 1
 if __name__== "__main__":
 	complete0()
 	if(main()==1):
