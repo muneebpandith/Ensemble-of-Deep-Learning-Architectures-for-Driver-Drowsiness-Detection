97e695c151294bac6437ab92d7ed33c389817d5c
MUNEEB AHMED <muneebpandith@gmail.com>
2020-04-03 14:01:31 +0530

    Create 1. uploadtarspliitedfiles.py

---
+++ 1. uploadtarspliitedfiles.py
@@ -0,0 +1,31 @@
+import http.client
+import os
+
+
+def complete0():
+        conn = http.client.HTTPSConnection("api.msg91.com")
+        payload = "{ \"sender\": \"SOCKET\", \"route\": \"4\", \"country\": \"91\", \"sms\": [ { \"message\": \"Starting:Uploading of tarred (splitted files-newsubjectwise) process is starting\", \"to\": [ \"9149429559\" ] }] }"
+        headers = {'authkey': "118364AVIfu09J5e85f50eP1",'content-type': "application/json"}
+        conn.request("POST", "/api/v2/sendsms", payload, headers)
+        res = conn.getresponse()
+        data = res.read()
+        print(data.decode("utf-8"))
+      
+def complete():
+	conn = http.client.HTTPSConnection("api.msg91.com")
+	payload = "{ \"sender\": \"SOCKET\", \"route\": \"4\", \"country\": \"91\", \"sms\": [ { \"message\": \"Complete: Uploading of tarred (splitted files-newsubjectwise) process is COMPLETE\", \"to\": [ \"9149429559\"] }] }"
+	headers = {'authkey': "118364AVIfu09J5e85f50eP1",'content-type': "application/json"}
+	conn.request("POST", "/api/v2/sendsms", payload, headers)
+	res = conn.getresponse()
+	data = res.read()
+	print(data.decode("utf-8"))
+
+
+def main():
+  #this excludes subject 005
+  os.system("tar -czvf '../../MTECHMINORLINUX/DATA/newsubjectwise.tar.gz' '../../MTECHMINORLINUX/DATA/newsubjectwise' >> tarofsplittedfiles.txt")
+  return 1
+if __name__== "__main__":
+	complete0()
+	if(main()==1):
+		complete()
