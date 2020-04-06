b6ca2b06fce7da3d0057a51486030d91621a1376
MUNEEB AHMED <muneebpandith@gmail.com>
2020-04-06 05:55:38 +0000

    Via VM Upload

---
+++ 1. uploadtarspliitedfiles.py
@@ -0,0 +1,43 @@
+
+
+
+
+import http.client
+import os
+import dropbox
+
+def complete0():
+        conn = http.client.HTTPSConnection("api.msg91.com")
+        payload = "{ \"sender\": \"SOCKET\", \"route\": \"4\", \"country\": \"91\", \"sms\": [ { \"message\": \"Starting:Uploading of tarred (splitted files-newsubjectwise) process is starting\", \"to\": [ \"9149429559\" ] }] }"
+        headers = {'authkey': "118364AVIfu09J5e85f50eP1",'content-type': "application/json"}
+        conn.request("POST", "/api/v2/sendsms", payload, headers)
+        res = conn.getresponse()
+        data = res.read()
+        print(data.decode("utf-8"))
+      
+def complete():
+	conn = http.client.HTTPSConnection("api.msg91.com")
+	payload = "{ \"sender\": \"SOCKET\", \"route\": \"4\", \"country\": \"91\", \"sms\": [ { \"message\": \"Complete: Uploading of tarred (splitted files-newsubjectwise) process is COMPLETE\", \"to\": [ \"9149429559\"] }] }"
+	headers = {'authkey': "118364AVIfu09J5e85f50eP1",'content-type': "application/json"}
+	conn.request("POST", "/api/v2/sendsms", payload, headers)
+	res = conn.getresponse()
+	data = res.read()
+	print(data.decode("utf-8"))
+
+def upload_file(file_from, file_to,access_token):
+	dbx = dropbox.Dropbox(access_token)
+	f = open(file_from, 'rb')
+	dbx.files_upload(f.read(), file_to)
+
+def main():
+	#this excludes subject 005
+	
+	access_token = 'pf60SIufNzAAAAAAAAAARMYjdOPQY4yCU1E9J5xb8OyOWiyBP1kcktdgHW7h9MC_'
+	file_from = '../../MTECHMINORLINUX/DATA/newsubjectwise.tar.gz'  
+	file_to = '/Nexus/newsubjectwise.tar.gz' 
+	upload_file(file_from,file_to,access_token)
+	return 1
+if __name__== "__main__":
+	complete0()
+	if(main()==1):
+		complete()
--- 2. annotate.py
+++ 2. annotate.py
@@ -14,7 +14,7 @@ def complete0():
         res = conn.getresponse()
         data = res.read()
         print(data.decode("utf-8"))
-       
+
 
 def complete():
         conn = http.client.HTTPSConnection("api.msg91.com")
@@ -24,7 +24,7 @@ def complete():
         res = conn.getresponse()
         data = res.read()
         print(data.decode("utf-8"))
-       
+
 def getannotation(filename):
 	fileobj = open(filename,"r")
 	filedata = fileobj.read()
@@ -46,25 +46,24 @@ def annotateimage(readingfile, writingdirectory, fileattrib):
 	else:
 		print(savingas)
 
-	
+
 	savingas=   writingdirectory+"/"+fileattrib['sname']+"."+ fileattrib['typeofface']+"."+fileattrib['tname']+"."+fileattrib['cname']+"."+fileattrib['annotation']+"."+fileattrib['frameno']+".jpg"
 	#print("Reading From: "+ readingfile + "Copying to: ", savingas)
-	
+
 	if (os.path.exists(savingas) and os.stat(savingas).st_size> 0):
 		print("Exists, So ignore: "+savingas)
 	else:
-		print(savingas)
-		cv2.imwrite(savingas,imgCroppedFace)
+		os.system("cp "+readingfile+" "+savingas +" >> anotationlog.txt")
 def main():
 
-	
+
 	mainpath= "../../MTECHMINORLINUX/DATA"
 	##STEP 1: FIND SUBJECTS e.g., '001','002', etc & COMBINATION TYPES e.g., 'glasses' etc. AND TYPES e.g., nonsleepyCombination etc.
 	#Give the paths respectively than manually typing 001 etc glasses etc, and sleepyComb... etc
-	
+
 
 	readingpath=mainpath+"/newsubjectwise"
-		
+
 	savingpath=mainpath+"/annotatednewsubjectwise"
 
 
@@ -74,22 +73,22 @@ def main():
 
 
 	subjects=os.listdir(subjectspath)
-	#['001', '002', '005', '006', '008', '009', '012', '013', '015', '020', '023', '024', '031', '032', '033', '034', '035', '036'] 
+	#['001', '002', '005', '006', '008', '009', '012', '013', '015', '020', '023', '024', '031', '032', '033', '034', '035', '036']
 	#subjects=['023']
 	#subjects=['023','024','031','032','033','034','035','036']
 	print(subjects)
-	
+
 
 
 	for eachsubject in subjects:
 		typespath= subjectspath+"/"+eachsubject
-		types   =os.listdir(typespath)	
+		types   =os.listdir(typespath)
 		#['glasses', 'nightglasses', 'night_noglasses', 'noglasses', 'sunglasses']
 		#print(types)
 
 
 
-		
+
 		for eachtype in types:
 			combspath= typespath+"/"+eachtype
 			combs   =os.listdir(combspath)
@@ -107,33 +106,33 @@ def main():
 				annotationdata=getannotation(annotationspath)
 				for eachfile in myfiles:
 					readingfrom= readingpath +"/"+eachsubject+"/"+eachtype+"/"+eachcomb+"/"+eachfile
-					
+
 					#subjectwise ordering
 					savingto=savingpath+"/"+ eachsubject +"/" +eachtype+"/"+eachcomb
-					
+
 					if not os.path.exists(savingto):
 						print("Creating Path for "+eachsubject)
-						os.system("mkdir -p "+savingto)	
+						os.system("mkdir -p "+savingto)
 
 					if(eachtype=='night_noglasses' or eachtype=='nightglasses'):
 						fps=15
 					else:
 						fps=30
-					
 
-					
+
+
 					#print(readingfileinfo)
 					#readingfrom=eachfile
 
 					fileattrib=dict()
-					
+
 					fileattrib['sname']=sname=eachfile.split(".")[0]
 					fileattrib['typeofface']=typeofface=eachfile.split(".")[1]
 					fileattrib['tname']=tname=eachfile.split(".")[2]
 					fileattrib['cname']=cname=eachfile.split(".")[3]
 					fileattrib['annotation']=annotation=eachfile.split(".")[4]
 					fileattrib['frameno']=frameno=eachfile.split(".")[5]
-					
+
 
 					fileattrib['annotation']=updateannotation(annotationdata,fileattrib['frameno'])
 					annotateimage(readingfrom, savingto, fileattrib)
@@ -142,7 +141,3 @@ if __name__== "__main__":
         complete0()
         if main()==1 :
                 complete()
-
-
-
-
